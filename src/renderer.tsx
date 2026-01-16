ac8cb5ead4234ee9ac2f38c74c1fadd6e4b7bf31
Omar <6778101+ohyjek@users.noreply.github.com>
2026-01-16 07:25:10 -0500

    format, more setup

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -25,17 +25,18 @@
  *  });
  * ```
  */
-import { render } from 'solid-js/web';
-import App from './components/App';
+import { render } from "solid-js/web";
+import App from "./components/App";
 
-import './index.css';
+import "./index.css";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html?',
+    "Root element not found. Did you forget to add it to your index.html?"
   );
 }
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 render(() => <App />, root!);
